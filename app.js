8cf6a1fd624686297aed8de64d9365081c33caf0
Min <79357141+mjtwo20@users.noreply.github.com>
2026-04-30 17:44:55 -0700

    Added app.js and modified index.html as feature1.

---
+++ app.js
@@ -0,0 +1,9 @@
+const contentText = document.querySelector(".conetent");
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    contentText.innerHTML = "This is the content";
+  },
+  false,
+);
